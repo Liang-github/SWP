38741ea6c215310f3f3443949a7a7493883b1401
LIANG <pl941227@icloud.com>
2017-05-25 11:10:58 +0800

    增加了一些文件

---
+++ app/reducers/rootRudcer.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ljunb on 16/5/25.
+ * 根reducer
+ */
+import { combineReducers } from 'redux';
+import Strolling from './strollingReducer';
+import Foods from './foodsReducer';
+import FoodsList from './foodsListReducer';
+import FoodCompare from './foodCompareReducer';
+import FoodInfo from './foodInfoReducer';
+import Search from './searchReducer';
+import User from './userReducer';
+
+export default rootReducer = combineReducers({
+    Strolling,
+    Foods,
+    FoodsList,
+    FoodCompare,
+    FoodInfo,
+    Search,
+    User,
+})
